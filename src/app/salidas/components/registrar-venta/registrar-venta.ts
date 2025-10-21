502b48985eab7a94ba03b991b917b137e5afbc1d
Totality123 <Steven2004medina@gmail.com>
2025-10-21 14:55:44 -0600

    14

--- src/app/salidas/components/registrar-venta/registrar-venta.ts
+++ src/app/salidas/components/registrar-venta/registrar-venta.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registrar-venta',
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink],
   templateUrl: './registrar-venta.html',
   styleUrl: './registrar-venta.css'
 })
