ae4965b207f52ffc906ba24979208ecf12b7869f
DerekMVA <derekmva2004@gmail.com>
2025-10-21 12:54:32 -0600

    5

--- src/app/entradas/components/lista-compras/lista-compras.ts
+++ src/app/entradas/components/lista-compras/lista-compras.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './lista-compras.html',
   styleUrl: './lista-compras.css'
 })
